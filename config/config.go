acc432f85d4796eabb6c7a350985f8b7c9489f31
Dmitry Gorlanov <dmitrygorlanov0@gmail.com>
2025-10-12 15:24:51 +0300

    update config

---
+++ config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type (
+	Config struct {
+		App     App
+		HTTP    HTTP
+		Log     Log
+		Swagger Swagger
+		PG      PG
+	}
+
+	App struct {
+		Name    string `env:"APP_NAME,required"`
+		Version string `env:"APP_VERSION,required"`
+	}
+
+	HTTP struct {
+		Port           string `env:"HTTP_PORT,required"`
+		UsePreforkMode bool   `env:"HTTP_USE_PREFORK_MODE" envDefault:"false"`
+	}
+
+	Log struct {
+		Level string `env:"LOG_LEVEL,required"`
+	}
+
+	Swagger struct {
+		Enabled bool `env:"SWAGGER_ENABLED" envDefault:"false"`
+	}
+
+	PG struct {
+		PoolMax int    `env:"PG_POOL_MAX,required"`
+		URL     string `env:"PG_URL,required"`
+	}
+)
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	return cfg, nil
+}
