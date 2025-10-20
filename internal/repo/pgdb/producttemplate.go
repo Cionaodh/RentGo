f69840c6b5d14f8e41e2ba6cd10a5652429dc0de
Dmitry Gorlanov <dmitrygorlanov0@gmail.com>
2025-10-20 19:19:18 +0300

    update createTemplate() on Repository layer

--- internal/repo/pgdb/producttemplate.go
+++ internal/repo/pgdb/producttemplate.go
@@ -2,11 +2,13 @@ package pgdb
 
 import (
 	"EasyRentGo/internal/entity"
-	repotype "EasyRentGo/internal/repo/repotypes"
+	rp "EasyRentGo/internal/repo/repotypes"
 	"EasyRentGo/pkg/postgres"
 	"context"
+	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
 type ProductTemplateRepo struct {
@@ -17,8 +19,31 @@ func NewProductTemplateRepo(pg *postgres.Postgres) *ProductTemplateRepo {
 	return &ProductTemplateRepo{pg}
 }
 
-func (pt *ProductTemplateRepo) Create(context.Context, repotype.CreateTemplateInput) (entity.ProductTemp, error) {
-	return entity.ProductTemp{}, nil
+func (pt *ProductTemplateRepo) Create(ctx context.Context, temp rp.CreateTemplateInput) (entity.ProductTemp, error) {
+	// Добавить строку, которая содержит
+	// (имя, описание, Цену)
+
+	sql := `
+		INSERT INTO template (name, description, price)
+		VALUES ($1, $2, $3)
+		RETURNING *;
+	`
+
+	rows, err := pt.Pool.Query(ctx, sql,
+		temp.Name,
+		temp.Description,
+		temp.Price,
+	)
+	if err != nil {
+		return entity.ProductTemp{}, fmt.Errorf("ProductTemplateRepo - Create - pt.Pool.Query: %w", err)
+	}
+
+	template, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[entity.ProductTemp])
+	if err != nil {
+		return entity.ProductTemp{}, fmt.Errorf("ProductTemplateRepo - Create - pgx.CollectExactlyOneRow: %w", err)
+	}
+
+	return template, nil
 }
 
 func (pt *ProductTemplateRepo) GetAll(context.Context) ([]entity.ProductTemp, error) {
