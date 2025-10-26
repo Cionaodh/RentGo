0aa744261c9000ffa47b750c31cdd0fd7ef017fb
Dmitry Gorlanov <dmitrygorlanov0@gmail.com>
2025-10-26 17:04:53 +0300

    update migration file

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -37,21 +37,21 @@ CREATE TABLE public.products(
    CONSTRAINT product_rentpoint_fkey FOREIGN KEY (rentpoint_id) 
       REFERENCES public.rentpoints(id) 
       ON DELETE SET NULL 
-      ON UPDATE CASCADE -- Добавлено действие для ON UPDATE
+      ON UPDATE CASCADE
 );
 
--- Заполнение начальными значениями
 INSERT INTO public.rentpoints (id, "name", addr) VALUES
    ('550e8400-e29b-41d4-a716-446655440001'::uuid, 'rentpoint1', 'First1'),
    ('550e8400-e29b-41d4-a716-446655440002'::uuid, 'rentpoint2', 'First2'),
    ('550e8400-e29b-41d4-a716-446655440003'::uuid, 'rentpoint3', 'First3');
 
 INSERT INTO public.templates (id, "name", description, price) VALUES 
-   ('550e8400-e29b-41d4-a716-446655550001'::uuid, 'template1', 'description1', 1500), -- Исправлены кавычки и опечатка
+   ('550e8400-e29b-41d4-a716-446655550001'::uuid, 'template1', 'description1', 1500),
    ('550e8400-e29b-41d4-a716-446655550002'::uuid, 'template2', 'description2', 2000),
    ('550e8400-e29b-41d4-a716-446655550003'::uuid, 'template3', 'description3', 1000),
    ('550e8400-e29b-41d4-a716-446655550004'::uuid, 'template4', 'description4', 3000);
 
--- Пример корректной вставки в products (при необходимости):
--- INSERT INTO public.products (id, status, template_id, rentpoint_id) VALUES
---    (uuid_generate_v4(), 'Free', '550e8400-e29b-41d4-a716-446655550001'::uuid, '550e8400-e29b-41d4-a716-446655440001'::uuid);
+INSERT INTO public.products (id, status, template_id, rentpoint_id) VALUES
+   (uuid_generate_v4(), 'Free', '550e8400-e29b-41d4-a716-446655550001'::uuid, '550e8400-e29b-41d4-a716-446655440001'::uuid),
+   (uuid_generate_v4(), 'Free', '550e8400-e29b-41d4-a716-446655550001'::uuid, '550e8400-e29b-41d4-a716-446655440001'::uuid),
+   (uuid_generate_v4(), 'Free', '550e8400-e29b-41d4-a716-446655550003'::uuid, '550e8400-e29b-41d4-a716-446655440001'::uuid);
